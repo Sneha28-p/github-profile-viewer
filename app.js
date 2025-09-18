060d612172a5b80d3dac970ad139ce5d9de00884
Sneha28-p <snehapatted28@gmail.com>
2025-09-19 00:11:08 +0530

    Made function showMessage()

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 async function getUser() {
-  let input = document.getElementById("username").value.trim();
+  const input = document.getElementById("username").value.trim();
 
   if (!input) {
-    document.getElementById("profile").innerHTML = "<p>Please enter a username or link ⚠️</p>";
+    showMessage("Please enter a username or link ⚠️")
     return;
   }
 
@@ -14,19 +14,20 @@ async function getUser() {
   }
 
   const url = `https://api.github.com/users/${username}`;
+  showMessage("⏳ Loading...");
 
   try {
     const response = await fetch(url);
 
     if (!response.ok) {
-      document.getElementById("profile").innerHTML = "<p>User not found ❌</p>";
+      showMessage("User not found ❌");
       return;
     }
 
     const user = await response.json();
     displayUser(user);
   } catch (error) {
-    document.getElementById("profile").innerHTML = "<p>Network error ❌</p>";
+    showMessage("Network error ❌");
     console.error("Error fetching user:", error);
   }
 }
@@ -34,7 +35,7 @@ async function getUser() {
 function displayUser(user) {
   document.getElementById("profile").innerHTML = `
     <div class="card">
-      <img src="${user.avatar_url}" width="120" style="border-radius:50%">
+      <img src="${user.avatar_url}" class="avatar">
       <h2>${user.name || "No Name Available"}</h2>
       <p>@${user.login}</p>
       <p>${user.bio || "No bio available"}</p>
@@ -43,4 +44,7 @@ function displayUser(user) {
       <a href="${user.html_url}" target="_blank">View Profile</a>
     </div>
   `;
+}
+function showMessage(msg) {
+  document.querySelector("#profile").innerHTML = `<p>${msg}</p>`;
 }
